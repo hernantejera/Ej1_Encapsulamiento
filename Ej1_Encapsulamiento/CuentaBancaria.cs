c65f463e021ab5f852e3ce61b23fd5e08c90a4ef
hernantejera <hernanmtejera@gmail.com>
2023-07-26 11:17:20 -0300

    Agregar archivos de proyecto.

---
+++ Ej1_Encapsulamiento/CuentaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1_Encapsulamiento
+{
+   // Crea una clase llamada "CuentaBancaria" con una propiedad privada "saldo"
+   // y métodos públicos como "Depositar" y "Retirar".
+   // Asegúrate de que estos métodos verifiquen y actualicen adecuadamente el saldo.
+    public  class CuentaBancaria
+    {
+        private double Saldo { get; set; }
+
+        public double Depositar(double Deposito) 
+        {
+            return Saldo += Deposito;
+        }
+
+        public string SaldoActualConDeposito() 
+        {
+            return $"Su saldo actual es {Saldo}";
+        }
+
+        public double Retirar(double Extraccion) 
+        {
+        return Saldo -= Extraccion;
+        }
+        public string SaldoActualConExtraccion() 
+        {
+            return $"Su saldo actual es {Saldo} ";
+        }
+    }
+}
---
+++ Ej1_Encapsulamiento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Ej1_Encapsulamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           CuentaBancaria cuentaBancaria = new CuentaBancaria();
+            cuentaBancaria.Depositar(800000);
+            Console.WriteLine(cuentaBancaria.SaldoActualConDeposito());
+
+            cuentaBancaria.Retirar(400000);
+            Console.WriteLine(cuentaBancaria.SaldoActualConExtraccion());
+        }
+    }
+}
